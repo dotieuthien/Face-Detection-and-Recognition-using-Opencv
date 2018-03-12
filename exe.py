e56602481e00b7d92a7c136667a6db8613e79407
dotieuthien <dotieuthien9997@gmail.com>
2018-03-13 00:39:39 +0700

    Done for recognition part

--- exe.py
+++ exe.py
@@ -6,35 +6,34 @@ Date: 23/1/2018
 """-----------------------------------------------------------------------------
 IMPORT 
 -----------------------------------------------------------------------------"""
-import cv2
 import os
 import numpy as np
 from func.create_dataset import create_dataset
 from func.training import training
-from func.detect_faces import detect_faces
+from func.DetectFaces import DetectFaces
 
 """-----------------------------------------------------------------------------
 MAIN 
 -----------------------------------------------------------------------------"""
-print("""Selecting mode:
-1. Creating new data 
-2. Detecting faces 
-3. Training data """)
+while(True):
+    print("""Selecting mode:
+    1. Creating new data 
+    2. Training data  
+    3. Detecting faces """)
 
-mode = int(input("Enter a number correspond with a mode: "))
+    mode = int(input("Enter a number correspond with a mode: "))
 
-if mode == 1:
-    print("Creating new data")
-    num_users = [os.path.join("dataset", folder) for folder in os.listdir("dataset")]
-    id = np.shape(num_users)[0] + 1
-    create_dataset(id)
+    if mode == 1:
+        print("Creating new data")
+        num_users = [os.path.join("dataset", folder) for folder in os.listdir("dataset")]
+        id = np.shape(num_users)[0] + 1
+        create_dataset(id)
 
-elif mode == 2:
-    print("Detecting faces")
-    detect_faces()
+    elif mode == 2:
+        print("Training")
+        training()
 
-elif mode == 3:
-    print("Training")
-    training()
-
+    elif mode == 3:
+        print("Detecting faces")
+        DetectFaces()
 
